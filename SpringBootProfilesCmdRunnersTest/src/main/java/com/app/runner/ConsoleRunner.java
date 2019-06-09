58e4bbdf665e01a635c65482c596bd97f6e3f957
vidyasagar <vsagar954@gmail.com>
2019-06-09 09:37:12 +0530

    CommandLine Runner commit

---
+++ SpringBootProfilesCmdRunnersTest/src/main/java/com/app/runner/ConsoleRunner.java
@@ -0,0 +1,16 @@
+package com.app.runner;
+
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+@Component
+public class ConsoleRunner implements CommandLineRunner {
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println(Arrays.asList(args));
+		
+	}
+
+}
